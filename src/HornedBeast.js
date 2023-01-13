fb5c2686ee12b808a810650f66308a5dc7a04312
Brenden Moore <brendenm3603@gmail.com>
2023-01-12 19:42:12 -0800

    added hornedBeast commponent with state and prop elements

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,47 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
+import Card from 'react-bootstrap/Card'
+
+
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clickedFavorite: false,
+      Count: 0
+    }
+  }
+
+
+  handleFavorite = () => {
+    this.setState({
+      clickedFavorite: true,
+      Count: this.state.Count + 1
+    
+    })
+  }
+
+
+
+
   render() {
     return (
-      <>
-      <h2>{this.props.title}</h2>
-      <img src={this.props.imgUrl} alt={this.props.alt} title={this.props.title}/>
-      <p>{this.props.description}</p>
-      </>
+      <div class="Beast" key={this.props.index}>
+
+        <Card style={{ width: '18rem', height: 'fit-content'}}>
+          <Card.Title as="h2" >{this.props.title}</Card.Title>
+          <Card.Img varient="top" src={this.props.image_url} alt={this.props.alt} title={this.props.title} onClick={this.handleFavorite}/>
+          <Card.Title as="h3" >Info:</Card.Title>
+          <Card.Text>Number of favorites {this.state.Count + "❤️"}</Card.Text>
+          <Card.Text>{this.props.description}</Card.Text>
+          <ListGroup>
+          <ListGroup.Item>Keyword: {this.props.keyword}</ListGroup.Item>
+          <ListGroup.Item> Number of horns: {this.props.horns}</ListGroup.Item>
+          </ListGroup>
+        </Card>
+        
+        </div>
     )
   }
 }
