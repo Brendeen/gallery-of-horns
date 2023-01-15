8cc68a31fc94fddbd646972d18119219bb68aafb
Brenden Moore <brendenm3603@gmail.com>
2023-01-14 17:06:00 -0800

    changed index to render app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
-    <Main/>
-    <Footer/>
+   <App/>
   </React.StrictMode>
 );
 
