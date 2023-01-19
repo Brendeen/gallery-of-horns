0b981d32303fb3bde8b1fdee89a174c463f90c4d
Brenden Moore <brendenm3603@gmail.com>
2023-01-18 22:45:53 -0800

    passed hornedData as a prop through data

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,12 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import data from "./data.json"
 import "./App.css"
 
 class Main extends React.Component {
   render() {
     return (
       <main>
-       {data.map((hornedBeast, index) => <HornedBeast 
+       {this.props.hornData.map((hornedBeast, index) => <HornedBeast 
        title={hornedBeast.title}
        image_url={hornedBeast.image_url} 
        description={hornedBeast.description} 
